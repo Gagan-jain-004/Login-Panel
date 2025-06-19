2b321355b82d628ba0538c8c382dd74595f9ff81
Gagan jain004 <gagan.praveen004@gmail.com>
2025-06-19 12:22:39 +0530

    minor error in frontend url in cors

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,9 @@ connectDB();
 app.use(cors({
     origin : [
          "http://localhost:5173",
-         "https://login-panel-eta.vercel.app/",
-         "https://login-panel-git-main-gagan-jain-004s-projects.vercel.app/",
-         "https://login-panel-gagan-jain-004s-projects.vercel.app/",
+         "https://login-panel-eta.vercel.app",
+         "https://login-panel-git-main-gagan-jain-004s-projects.vercel.app",
+         "https://login-panel-gagan-jain-004s-projects.vercel.app",
 
     ]
     ,credentials:true
