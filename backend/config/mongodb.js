9a3cc9fc0c6c63c70ffa8ec54e9962f37abf8f98
Gagan jain004 <gagan.praveen004@gmail.com>
2025-06-21 14:19:19 +0530

    unused removed

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     mongoose.connection.on('connected',()=> console.log("Db connected successfully"));
 
-    await mongoose.connect(`${process.env.MONGO_URI}/Login-Portal`);         //mern-auth is project name    
+    await mongoose.connect(`${process.env.MONGO_URI}/Login-Portal`);        
 };
 
 export default connectDB;
